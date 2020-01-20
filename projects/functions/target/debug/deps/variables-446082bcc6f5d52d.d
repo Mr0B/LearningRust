f82dc74bca5c63aa4054f3ce8553918d6862649b
Mr0B <luca.orengobrando@gmail.com>
2020-01-20 16:21:11 +0100

    Ceci est un test

---
+++ projects/functions/target/debug/deps/variables-446082bcc6f5d52d.d
@@ -0,0 +1,5 @@
+/home/wics/LearningRust/projects/functions/target/debug/deps/variables-446082bcc6f5d52d: src/main.rs
+
+/home/wics/LearningRust/projects/functions/target/debug/deps/variables-446082bcc6f5d52d.d: src/main.rs
+
+src/main.rs:
---
+++ projects/functions/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/wics/LearningRust/projects/functions/target/debug/variables: /home/wics/LearningRust/projects/functions/src/main.rs
